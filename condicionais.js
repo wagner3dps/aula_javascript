f2853d17213beea472e2b218f8943dc2d0c9b56d
wagner3dps <35384095+wagner3dps@users.noreply.github.com>
2019-09-06 17:15:12 -0300

    Add files via upload

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+
+
+// Condicionais
+
+
+/*
+
+var idade = prompt("Qual é a sua idade?");
+if (idade >= 18) {
+	alert('Você é "Maior de Idade"');
+}else{
+	alert('Você é "Menor de Idade"');	
+};
+
+*/
+
+
---
+++ date.js
@@ -0,0 +1,90 @@
+
+
+// Date
+
+
+/*
+
+// Data + Hora + Fuso horario
+
+var d = new Date();
+alert(d);
+console.log(d);
+
+*/
+
+
+/*
+
+// Dia (o computador sempre começa a contar do zero, é imprescindível acrescentar +1) 
+
+var d = new Date();
+alert(d.getDay()+1);
+console.log(d.getDay()+1);
+
+*/
+
+
+/*
+
+// Mês (o computador sempre começa a contar do zero, é imprescindível acrescentar +1) 
+
+var d = new Date();
+alert(d.getMonth()+1);
+console.log(d.getMonth()+1);
+
+*/
+
+
+/*
+
+// Horas 
+
+var d = new Date();
+alert(d.getHours());
+console.log(d.getHours());
+
+*/
+
+
+/*
+
+// Minutos 
+
+var d = new Date();
+alert(d.getMinutes());
+console.log(d.getMinutes());
+
+*/
+
+
+/*
+
+// Segundos 
+
+var d = new Date();
+alert(d.getSeconds());
+console.log(d.getSeconds());
+
+*/
+
+
+/*
+
+// Milissegundos 
+
+var d = new Date();
+alert(d.getMilliseconds());
+console.log(d.getMilliseconds());
+
+*/
+
+
+
+
+ 
+
+
+
+
+
